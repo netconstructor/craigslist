db09be93ab13f14b3c27bb5717f6d83bba417b2f
Apprentice <apprentice@dbc31.local>
2012-10-30 17:11:54 -0700

    Update dbsaver to work with new schema

--- dbsaver.rb
+++ dbsaver.rb
@@ -3,14 +3,20 @@ require 'sqlite3'
 module DBSaver
   extend self
 
-  def save(object)
+  def save(search_result)
     open_database
-    case object
+    case search_result
     when SearchResult
-      save_search_result(object)
-      save_postings(object)
+      # save_search_result(object) unless search_result_exists?(object)
+      search_result_exists?(search_result) ? update_search_result_time(search_result) : save_search_result(search_result)
+      save_postings(search_result)
     end
   end
+
+  # def update(object)
+  #   open_database
+  #   update_search_result(object)
+  # end
 
 
   private
@@ -18,28 +24,62 @@ module DBSaver
     @db = SQLite3::Database.new('./craigslist.db')
   end
 
-  def save_search_result(search_result)
-    @db.execute("INSERT INTO search_results (search_query, search_url, scraped_at, created_at, updated_at)
-                VALUES (?, ?, ?, DATETIME('now'),DATETIME('now'))", search_result_attributes(search_result))
+  def update_search_result_time(search_result)
+    @db.execute("UPDATE search_results SET updated_at=DATETIME('now') WHERE search_url='#{search_result.url}'")
   end
+
+  # def update_postings(posting)
+  #   @db.execute("UPDATE postings")
+  # end
+
+  def save_search_result(search_result)
+    @db.execute("INSERT INTO search_results (search_query, search_url, scraped_at, created_at, updated_at, user_email)
+                VALUES (?, ?, ?, DATETIME('now'),DATETIME('now'), ?)", search_result_attributes(search_result))
+  end
+
 
   def save_postings(search_result)
     search_result.postings.each do |posting|
-      save_posting(posting)
+      unless posting_exists?(posting)
+        save_posting(posting)  #postings table
+        save_search_result_posting(posting, search_result) #join table (many<->many) search_result_postings
+      end
     end
   end
 
   def save_posting(posting)
     @db.execute(
-      "INSERT INTO postings (title, date_posted, price, location, url, search_result_id,created_at,updated_at)
-      VALUES (?, ?, ?, ?, ?, ?, DATETIME('now'), DATETIME('now'))", posting_attributes(posting))
+      "INSERT INTO postings (title, date_posted, price, location, url,created_at,updated_at)
+      VALUES (?, ?, ?, ?, ?, DATETIME('now'), DATETIME('now'))", posting_attributes(posting))
+  end
+
+  def save_search_result_posting(posting, search_result)
+    @db.execute(
+    "INSERT INTO search_result_postings (posting_id, search_result_id)
+    VALUES (?, ?)", posting_id(posting), search_result_id(search_result)
+    )
+  end
+
+  def posting_id(posting)
+    @db.execute(
+     "SELECT id FROM postings
+      WHERE url='#{posting.url}'").flatten.first
+  end
+
+  def posting_exists?(posting)
+    @db.execute("SELECT url FROM postings WHERE url='#{posting.url}'").flatten.length > 0
+  end
+
+  def search_result_exists?(search_result)
+    @db.execute("SELECT id FROM search_results WHERE search_url='#{search_result.url}' AND user_email='#{search_result.email}'").flatten.length > 0
   end
 
   def search_result_attributes(search_result)
     search_query = search_result.search_query
-    search_url = search_result.url
-    scraped_at = search_result.time_scraped
-    [search_query, search_url, scraped_at]
+    search_url   = search_result.url
+    scraped_at   = search_result.time_scraped
+    email        = search_result.email
+    [search_query, search_url, scraped_at, email]
   end
 
   def posting_attributes(posting)
@@ -48,11 +88,15 @@ module DBSaver
     price = posting.price
     location = posting.location
     url = posting.url
-    [title, date_posted, price, location, url, search_result_id]
+    [title, date_posted, price, location, url]
   end
 
-  def search_result_id
-    @search_result_id ||= @db.execute("select last_insert_rowid();").first.first
+  def search_result_id(search_result)
+    @db.execute(
+    "SELECT id FROM search_results
+     WHERE user_email='#{search_result.email}'
+     AND search_url='#{search_result.url}'").flatten.last
   end
+
 end
 
